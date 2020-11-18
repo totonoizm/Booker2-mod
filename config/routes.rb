0795d3540f723a06b0c2220f82407b8d9a93697d
EC2 Default User <ec2-user@ip-172-31-44-141.ec2.internal>
2020-11-18 13:23:07 +0000

    favoritemodel作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'books#top'
   resources :books do
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
     resources :users, only: [:index, :show, :edit, :destroy, :update  ]
 

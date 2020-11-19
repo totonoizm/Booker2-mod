04796f3c5659462d852965bf33c85d02e0bb9462
EC2 Default User <ec2-user@ip-172-31-44-141.ec2.internal>
2020-11-19 07:34:03 +0000

    アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites
   has_many :book_comments, dependent: :destroy
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy #フォロー取得
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy #フォロワー取得
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人を取得
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人を取得
+
   attachment :profile_image
 
   validates :name, presence: true, length: {minimum: 2, maximum: 20}

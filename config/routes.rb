96eded05cf242d32e17216cb50803f5d53a9c368
EC2 Default User <ec2-user@ip-172-31-44-141.ec2.internal>
2020-11-19 08:39:46 +0000

    userindexフォロー機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get '/users/sign_out' => 'devise/sessions#destroy'
   get 'home/about' => 'books#about'
   get 'books/top' => 'books#top'
-  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
-  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+  post 'follow/:id' => 'relationships#create', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#destroy', as: 'unfollow' # フォロー外す
   devise_for :users
   root 'books#top'
   resources :books do

84c8d551adab1b4fa6f7701bedb9087febc8644a
EC2 Default User <ec2-user@ip-172-31-44-141.ec2.internal>
2020-11-19 08:05:38 +0000

    post followルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   get '/users/sign_out' => 'devise/sessions#destroy'
   get 'home/about' => 'books#about'
   get 'books/top' => 'books#top'
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
   devise_for :users
   root 'books#top'
   resources :books do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-    resources :users, only: [:index, :show, :edit, :destroy, :update  ]
-
+  resources :users, only: [:index, :show, :edit, :destroy, :update  ]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

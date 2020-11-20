d30309a94ec2214441d8952c2e540d91806e7cc1
EC2 Default User <ec2-user@ip-172-31-44-141.ec2.internal>
2020-11-20 21:40:31 +0000

    フォロー一覧作成開始

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy #フォロー取得
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy #フォロワー取得
-  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人を取得
-  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人を取得
+  has_many :following_user, through: :follower, source: :followed # 自分"が"フォローしている人を取得
+  has_many :follower_user, through: :followed, source: :follower # 自分"を"フォローしている人を取得
 
   # ユーザーをフォローする
 def follow(user_id)

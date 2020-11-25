8327565c8385e38748beb8be3859eb4b58324f25
EC2 Default User <ec2-user@ip-172-31-35-226.ec2.internal>
2020-11-25 04:48:24 +0000

    住所入力ログイン完成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_040709) do
+ActiveRecord::Schema.define(version: 2020_11_25_043112) do
 
   create_table "book_comments", force: :cascade do |t|
     t.integer "user_id"
@@ -59,8 +59,8 @@ ActiveRecord::Schema.define(version: 2020_11_25_040709) do
     t.integer "postcode"
     t.integer "prefecture_code"
     t.string "address_city"
-    t.string "adress_stereet"
-    t.string "adress_building"
+    t.string "address_street"
+    t.string "address_building"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
